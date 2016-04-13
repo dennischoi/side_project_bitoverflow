59ef561349697ee10a3f40c47e247f2e04d515d0
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-04-13 19:57:50 -0400

    added validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name, :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
   
+
+
 end
